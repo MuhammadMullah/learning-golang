b0a636fb40a2fb6bb723fe4fbee42ea27891d06c
MuhammadMullah <muhammadmullah16@gmail.com>
2020-02-10 15:05:34 +0300

    ready.... set... code.....!!!

---
+++ hello-go/main.go
@@ -0,0 +1,13 @@
+// this shows that this is an executable file. It must have a function main
+package main
+
+// getting access to all functionality of the "fmt" to this file
+// fmt is name of a standard library package that is included with
+// the go programming language by default. "ftm" short for format.
+
+// the library is used to print out info to the terminal
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
