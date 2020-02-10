7fd19e837a999556d51f344c3a909eadca11b041
MuhammadMullah <muhammadmullah16@gmail.com>
2020-02-10 15:37:40 +0300

    variable declarations

---
+++ cards/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// create a variable. We declare and assign a value to variable called (card)
+	// var card string = "Ace Of Spades"
+	card := "Ace Of Spades"
+
+	var number int = 123
+
+	fmt.Println(number)
+	// Print the variable
+	fmt.Println(card)
+}
+
+///////////// LESSONS LEARNT /////////////
+
+// 1. Go is a static typed language
+// 2. you can declare variables in golang using :=
+// 3. you can only use := when declaring a new variable and not when reassigning its value
